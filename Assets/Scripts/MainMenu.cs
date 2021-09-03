b1894e3b58acf27dc941c7776f0952ed9c8708ee
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-09-03 11:33:27 -0500

    FInal Changes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,10 +9,15 @@ public class MainMenu : MonoBehaviour
     public Material trapMat;
     public Material goalMat;
     public Toggle colorblindMode;
+	public Button Play;
+	public Button Quit;
+	public Button Options;
+
     // Start is called before the first frame update
     void Start()
     {
-
+		Play.onClick.AddListener(PlayMaze);
+		Quit.onClick.AddListener(QuitMaze);
     }
 
     // Update is called once per frame
@@ -23,11 +28,18 @@ public class MainMenu : MonoBehaviour
 
     public void PlayMaze()
     {
+		Debug.Log("Button pressed");
+
         if (colorblindMode.isOn)
         {
             trapMat.color = new Color32(255, 112, 0, 1);
             goalMat.color = Color.blue;
         }
+		else
+		{
+			trapMat.color = Color.red;
+			goalMat.color = Color.green;
+		}
         SceneManager.LoadScene("Maze");
     }
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,6 +29,11 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
 
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			SceneManager.LoadScene("Menu");
+		}
+
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
 		rb.AddForce(movement * speed);
